5b15380adf185f892a5c29d16764ae9d4a668b55
hynesenberg <noelhynes46@hotmail.com>
2021-04-08 00:31:15 +0100

    "Draft 3"

--- UCDPA_NoelHynes.py
+++ UCDPA_NoelHynes.py
@@ -13,6 +13,7 @@ Sp500_Time_new = Sp500_Time.rename(columns={'Name': 'Symbol'})
 
 print(Sp500_Time_new.head())
 
+Sp500_10 = Sp500_Time_new.merge(Sp500_Detail, on='Symbol', how='left')
 Sp500 = Sp500_Time_new.merge(Sp500_Detail, on='Symbol', how='left')
 print(Sp500.head())
 
@@ -20,25 +21,35 @@ Sp500_last = Sp500.drop_duplicates(subset=['Symbol'], keep='last')
 print(Sp500_last)
 
 Sp500_last.plot(kind='scatter', x= 'close', y='Symbol')
-plt.show()
-
-#Sp500_last.plot(kind='bar', x='Country', y='Symbol')
 #plt.show()
+
+
 ax = Sp500_last['Country'].value_counts().plot.bar()
 _ = ax.set_xlabel('Owner')
 _ = ax.set_ylabel('Frequency')
 
-plt.show()
+#plt.show()
 
 Sp500_last.plot(kind='scatter', x= 'Founded', y='Symbol')
-plt.show()
+#plt.show()
 
 
 
 trx = Sp500_last['GICS Sector'].value_counts().plot.bar(color=['grey', 'c','red' ,'peru', 'lightpink', 'yellow', 'orange','blue', 'purple', 'green', 'lavender'])
-plt.show()
+#plt.show()
 
 Sp500_sort=Sp500_last.sort_values(by=['close'])
 Sp500_top10 = Sp500_sort.tail(10)
 
-print(Sp500_top10['Symbol'])
+Top10 =[]
+Top10 = Sp500_top10['Symbol'].tolist()
+print(Top10)
+
+
+Sp500_T10 = Sp500_10[Sp500_10['Symbol'].isin(Top10)]
+print(Sp500_T10)
+
+Sp500_T10["date"] = pd.to_datetime(Sp500_T10["date"])
+Sp500_T10.pivot(index="date", columns="Symbol", values="close").plot()
+
+plt.show()
